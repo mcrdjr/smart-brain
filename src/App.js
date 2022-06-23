83762810efdbd916c6fec88ebb35430656282465
Michael Rucker <mcrdjr@sfdx2max.com>
2022-06-22 23:47:50 -0400

    Smart Brain

--- src/App.js
+++ src/App.js
@@ -54,6 +54,7 @@ class App extends Component {
   }
 
   calculateFaceLocation = (data) => {
+    console.log('data:', data)
     const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
     const image = document.getElementById('inputimage');
     const width = Number(image.width);
